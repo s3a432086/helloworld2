7514674933ac92bc3a3c46567a6d33eedbf925c9
Hongwei <tim5556618@gmail.com>
2022-10-01 03:38:40 +0800

    ver10.8

--- src/js/main.js
+++ src/js/main.js
@@ -190,6 +190,7 @@ jQuery(document).ready(function( $ ) {
   
   $('#btn-create-new-order').on('click', () => {
       this.http.get('https://jacktechtw.com/back.php?TABLE=TEST&USER=steve').subscribe(data => {
+        var obj = JSON.parse(data);
         alert(obj.USER.text());
         // var obj = JSON.parse(data);
         // alert(obj.USER);
