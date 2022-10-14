7e81f7e364bd3f007947f02eeb535a2baa6dd1c3
Hongwei <tim5556618@gmail.com>
2022-10-14 19:51:17 +0800

    ver17

--- src/js/main.js
+++ src/js/main.js
@@ -209,32 +209,429 @@ jQuery(document).ready(function( $ ) {
 
 
   
-  $("#flipbook").turn({
-    width: 400,
-    height: 300,
-    autoCenter: true
-});
+//   $("#flipbook").turn({
+//     width: 800,
+//     height: 600,
+//     autoCenter: true
+// });
 
 
 
 
-$(window).resize(function(e){
-// window.addEventListener('resize',function(e){
-  flipbook.style.width="";
-  flipbook.style.height="";
-  $(flipbook).turn("size", window.innerWidth*0.5,window.innerHeight*0.8);
-});   
+// $(window).resize(function(e){
+// // window.addEventListener('resize',function(e){
+//   flipbook.style.width="";
+//   flipbook.style.height="";
+//   $(flipbook).turn("size", window.innerWidth*0.5,window.innerHeight*0.8);
+// });   
   
 
 });
 
 
 
+function loadApp() {
+
+  $('#canvas').fadeIn(1000);
+
+  var flipbook = $('.magazine');
+
+  // Check if the CSS was already loaded
+ 
+ if (flipbook.width()==0 || flipbook.height()==0) {
+   setTimeout(loadApp, 10);
+   return;
+ }
+ 
+ // Create the flipbook
+
+ flipbook.turn({
+     
+     // Magazine width
+
+     width: 922,
+
+     // Magazine height
+
+     height: 600,
+
+     // Duration in millisecond
+
+     duration: 1000,
+
+     // Hardware acceleration
+
+     acceleration: !isChrome(),
+
+     // Enables gradients
+
+     gradients: true,
+     
+     // Auto center this flipbook
+
+     autoCenter: true,
+
+     // Elevation from the edge of the flipbook when turning a page
+
+     elevation: 50,
+
+     // The number of pages
+
+     pages: 12,
+
+     // Events
+
+     when: {
+       turning: function(event, page, view) {
+         
+         var book = $(this),
+         currentPage = book.turn('page'),
+         pages = book.turn('pages');
+     
+         // Update the current URI
+
+         Hash.go('page/' + page).update();
+
+         // Show and hide navigation buttons
+
+         disableControls(page);
+         
+
+         $('.thumbnails .page-'+currentPage).
+           parent().
+           removeClass('current');
+
+         $('.thumbnails .page-'+page).
+           parent().
+           addClass('current');
+
+
+
+       },
+
+       turned: function(event, page, view) {
+
+         disableControls(page);
+
+         $(this).turn('center');
+
+         if (page==1) { 
+           $(this).turn('peel', 'br');
+         }
+
+       },
+
+       missing: function (event, pages) {
+
+         // Add pages that aren't in the magazine
+
+         for (var i = 0; i < pages.length; i++)
+           addPage(pages[i], $(this));
+
+       }
+     }
+
+ });
+
+ // Zoom.js
+
+ $('.magazine-viewport').zoom({
+   flipbook: $('.magazine'),
+
+   max: function() { 
+     
+     return largeMagazineWidth()/$('.magazine').width();
+
+   }, 
+
+   when: {
+
+     swipeLeft: function() {
+
+       $(this).zoom('flipbook').turn('next');
+
+     },
+
+     swipeRight: function() {
+       
+       $(this).zoom('flipbook').turn('previous');
+
+     },
+
+     resize: function(event, scale, page, pageElement) {
+
+       if (scale==1)
+         loadSmallPage(page, pageElement);
+       else
+         loadLargePage(page, pageElement);
+
+     },
+
+     zoomIn: function () {
+
+       $('.thumbnails').hide();
+       $('.made').hide();
+       $('.magazine').removeClass('animated').addClass('zoom-in');
+       $('.zoom-icon').removeClass('zoom-icon-in').addClass('zoom-icon-out');
+       
+       if (!window.escTip && !$.isTouch) {
+         escTip = true;
+
+         $('<div />', {'class': 'exit-message'}).
+           html('<div>Press ESC to exit</div>').
+             appendTo($('body')).
+             delay(2000).
+             animate({opacity:0}, 500, function() {
+               $(this).remove();
+             });
+       }
+     },
+
+     zoomOut: function () {
+
+       $('.exit-message').hide();
+       $('.thumbnails').fadeIn();
+       $('.made').fadeIn();
+       $('.zoom-icon').removeClass('zoom-icon-out').addClass('zoom-icon-in');
+
+       setTimeout(function(){
+         $('.magazine').addClass('animated').removeClass('zoom-in');
+         resizeViewport();
+       }, 0);
+
+     }
+   }
+ });
+
+ // Zoom event
+
+ if ($.isTouch)
+   $('.magazine-viewport').bind('zoom.doubleTap', zoomTo);
+ else
+   $('.magazine-viewport').bind('zoom.tap', zoomTo);
+
+
+ // Using arrow keys to turn the page
+
+ $(document).keydown(function(e){
+
+   var previous = 37, next = 39, esc = 27;
+
+   switch (e.keyCode) {
+     case previous:
+
+       // left arrow
+       $('.magazine').turn('previous');
+       e.preventDefault();
+
+     break;
+     case next:
+
+       //right arrow
+       $('.magazine').turn('next');
+       e.preventDefault();
+
+     break;
+     case esc:
+       
+       $('.magazine-viewport').zoom('zoomOut');	
+       e.preventDefault();
+
+     break;
+   }
+ });
+
+ // URIs - Format #/page/1 
+
+ Hash.on('^page\/([0-9]*)$', {
+   yep: function(path, parts) {
+     var page = parts[1];
+
+     if (page!==undefined) {
+       if ($('.magazine').turn('is'))
+         $('.magazine').turn('page', page);
+     }
+
+   },
+   nop: function(path) {
+
+     if ($('.magazine').turn('is'))
+       $('.magazine').turn('page', 1);
+   }
+ });
+
+
+ $(window).resize(function() {
+   resizeViewport();
+ }).bind('orientationchange', function() {
+   resizeViewport();
+ });
+
+ // Events for thumbnails
+
+ $('.thumbnails').click(function(event) {
+   
+   var page;
+
+   if (event.target && (page=/page-([0-9]+)/.exec($(event.target).attr('class'))) ) {
+   
+     $('.magazine').turn('page', page[1]);
+   }
+ });
+
+ $('.thumbnails li').
+   bind($.mouseEvents.over, function() {
+     
+     $(this).addClass('thumb-hover');
+
+   }).bind($.mouseEvents.out, function() {
+     
+     $(this).removeClass('thumb-hover');
+
+   });
+
+ if ($.isTouch) {
+ 
+   $('.thumbnails').
+     addClass('thumbanils-touch').
+     bind($.mouseEvents.move, function(event) {
+       event.preventDefault();
+     });
+
+ } else {
+
+   $('.thumbnails ul').mouseover(function() {
+
+     $('.thumbnails').addClass('thumbnails-hover');
+
+   }).mousedown(function() {
+
+     return false;
+
+   }).mouseout(function() {
+
+     $('.thumbnails').removeClass('thumbnails-hover');
+
+   });
+
+ }
+
+
+ // Regions
+
+ if ($.isTouch) {
+   $('.magazine').bind('touchstart', regionClick);
+ } else {
+   $('.magazine').click(regionClick);
+ }
+
+ // Events for the next button
+
+ $('.next-button').bind($.mouseEvents.over, function() {
+   
+   $(this).addClass('next-button-hover');
+
+ }).bind($.mouseEvents.out, function() {
+   
+   $(this).removeClass('next-button-hover');
+
+ }).bind($.mouseEvents.down, function() {
+   
+   $(this).addClass('next-button-down');
+
+ }).bind($.mouseEvents.up, function() {
+   
+   $(this).removeClass('next-button-down');
+
+ }).click(function() {
+   
+   $('.magazine').turn('next');
+
+ });
+
+ // Events for the next button
+ 
+ $('.previous-button').bind($.mouseEvents.over, function() {
+   
+   $(this).addClass('previous-button-hover');
+
+ }).bind($.mouseEvents.out, function() {
+   
+   $(this).removeClass('previous-button-hover');
+
+ }).bind($.mouseEvents.down, function() {
+   
+   $(this).addClass('previous-button-down');
+
+ }).bind($.mouseEvents.up, function() {
+   
+   $(this).removeClass('previous-button-down');
+
+ }).click(function() {
+   
+   $('.magazine').turn('previous');
+
+ });
+
+
+ resizeViewport();
+
+ $('.magazine').addClass('animated');
+
+}
+
+// Zoom icon
+
+$('.zoom-icon').bind('mouseover', function() { 
+  
+  if ($(this).hasClass('zoom-icon-in'))
+    $(this).addClass('zoom-icon-in-hover');
+
+  if ($(this).hasClass('zoom-icon-out'))
+    $(this).addClass('zoom-icon-out-hover');
+
+}).bind('mouseout', function() { 
+  
+   if ($(this).hasClass('zoom-icon-in'))
+    $(this).removeClass('zoom-icon-in-hover');
+  
+  if ($(this).hasClass('zoom-icon-out'))
+    $(this).removeClass('zoom-icon-out-hover');
+
+}).bind('click', function() {
+
+  if ($(this).hasClass('zoom-icon-in'))
+    $('.magazine-viewport').zoom('zoomIn');
+  else if ($(this).hasClass('zoom-icon-out'))	
+   $('.magazine-viewport').zoom('zoomOut');
+
+});
+
+$('#canvas').hide();
+
+
+// Load the HTML4 version if there's not CSS transform
+
+// yepnope({
+//  test : Modernizr.csstransforms,
+//  yep: ['../lib/jquery/turn.js'],
+//  nope: ['../lib/jquery/turn.html4.min.js'],
+//  both: ['../lib/jquery/zoom.min.js', '../js/magazine.js', '../styles.scss'],
+//  complete: loadApp
+// });
+
+
+
+
+
+/*  先拿掉了
+
 
 // var url = 'E:\軟體開發\Angular\helloworld2\src\pdf\DM_V1.pdf'; //你要放的pdf檔
 // var url = 'https://jacktechtw.com/pdf/DM_V1.pdf'; //你要放的pdf檔
 // var url = 'https://github.com/s3a432086/helloworld2/raw/master/src/pdf/DM_V1.pdf.pdf'; //你要放的pdf檔
 var url = '../pdf/DM_V1.pdf'; //你要放的pdf檔
+var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';  //本機測試用
 var flipbook = document.getElementById('flipbook');
 var scale = 1.5; //可靠這個修改canvas解析度
 
@@ -249,7 +646,8 @@ window.onload = function(){
 function getpdf(url){
 
   var pdfjsLib = window['pdfjs-dist/build/pdf'];
-  pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';  // 若是下載在自己的本端，這邊src為自己專案中worker.js的路徑
+  // pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';  // 若是下載在自己的本端，這邊src為自己專案中worker.js的路徑
+  pdfjsLib.GlobalWorkerOptions.workerSrc = '../lib/jquery/pdf.worker.js'; 
 
   var loadingTask = pdfjsLib.getDocument(url);
   
@@ -277,24 +675,76 @@ function setcanvas(i,pdf,id){
     let canvas = document.getElementById(id);
     let context = canvas.getContext('2d');
 
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
+    // canvas.height = viewport.height;
+    // canvas.width = viewport.width;
 
-    // Render PDF page into canvas context
-    let renderContext = {
-    canvasContext: context,
-    viewport: viewport
-    };
+    // // Render PDF page into canvas context
+    // let renderContext = {
+    // canvasContext: context,
+    // viewport: viewport
+    // };
 
-    page.render(renderContext);
+    // page.render(renderContext);
+    //根据宽高判断是否需要旋转
+    if (viewport.height / viewport.width >= 1.42) {
+
+      var newScale = 1440 / viewport.height
+      var newViewport = page.getViewport({scale:newScale,rotation:rotate})
+      var outputScale = window.devicePixelRatio
+
+      canvas.width = Math.floor(newViewport.width * outputScale);
+      canvas.height = Math.floor(newViewport.height * outputScale);
+      canvas.style.width = Math.floor(newViewport.width) + "px";
+      canvas.style.height = Math.floor(newViewport.height) + "px";
+
+      var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] :
+        null;
+
+      var renderContext = {
+        canvasContext: context,
+        transform: transform,
+        viewport: newViewport
+        }
+         page.render(renderContext)
+
+      return ;
+    }
+
+    //根据每张图的宽高&#xff0c;按标准重新计算缩放比例
+    var newScale = 900 / viewport.height
+    
+    var newViewport = page.getViewport({scale:newScale})
+    var outputScale = window.devicePixelRatio
+
+    canvas.width = Math.floor(newViewport.width * outputScale);
+    canvas.height = Math.floor(newViewport.height * outputScale);
+    canvas.style.width = Math.floor(newViewport.width) + "px";
+    canvas.style.height = Math.floor(newViewport.height) + "px";
+
+    var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] :
+      null;
+
+      var renderContext = {
+          canvasContext: context,
+      transform: transform,
+          viewport: newViewport
+      }
+     page.render(renderContext)
   })
+
 };
 		
 function loadApp() {
-        $(flipbook).turn({
-          autoCenter: true,//是否置中
-          //display: 'single',//單頁顯示
-        });
+        // $(flipbook).turn({
+        //   autoCenter: true,//是否置中
+        //   // display: 'single',//單頁顯示
+        // });
+        //调用turn
+			yepnope({
+        test : Modernizr.csstransforms,
+        yep: ['../lib/jquery/turn.min.js'],
+        complete: loadApp
+      })
 };
 
 // $(window).resize(function(e){
@@ -303,3 +753,5 @@ window.addEventListener('resize',function(e){
   flipbook.style.height="";
   $(flipbook).turn("size", window.innerWidth*0.5,window.innerHeight*0.8);
 });
+
+*/
